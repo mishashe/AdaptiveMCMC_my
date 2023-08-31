a6ca41ca5499392f15e6962c50b34a64d9101104
Michael Sheinman <52411283+mishashe@users.noreply.github.com>
2023-08-31 14:07:27 +0300

    Update am.jl

--- src/am.jl
+++ src/am.jl
@@ -70,8 +70,8 @@ end
     sa.m .+= gamma.*sa.dx
     rmul!(sa.dx, sqrt(gamma))
     rmul!(sa.L.factors, sqrt(1.0-gamma))
-    println(sa.L)
-    println(sa.dx)
+    println(sa.x)
+    println(sa.m)
     lowrankupdate!(sa.L, sa.dx)
     println(55)
     nothing
